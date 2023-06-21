a16cdad76a82d50d067ba01943fb69e0a5bee5fa
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2023-06-21 10:31:01 +0200

    Add logging info

--- install_mirte.sh
+++ install_mirte.sh
@@ -5,13 +5,13 @@ MIRTE_SRC_DIR=/usr/local/src/mirte
 
 # Update
 sudo apt update
-
-# Install locales
-sudo apt install -y locales
-sudo locale-gen "nl_NL.UTF-8"
-sudo locale-gen "en_US.UTF-8"
-sudo update-locale LC_ALL=en_US.UTF-8 LANGUAGE=en_US.UTF-8
-
+sudo apt install -y locales python3.8 python3-pip python3-setuptools
+{
+	# Install locales
+	sudo locale-gen "nl_NL.UTF-8"
+	sudo locale-gen "en_US.UTF-8"
+	sudo update-locale LC_ALL=en_US.UTF-8 LANGUAGE=en_US.UTF-8
+} 2>&1 | sed 's/^/locales::: /' &
 # Install vcstool
 cp repos.yaml $MIRTE_SRC_DIR
 cp download_repos.sh $MIRTE_SRC_DIR
@@ -19,53 +19,54 @@ cd $MIRTE_SRC_DIR
 ./download_repos.sh
 
 # Install dependecnies to be able to run python3.8
-sudo apt install -y python3.8 python3-pip python3-setuptools
+sudo apt install -y
 
 # Set piwheels as pip repo
 sudo bash -c "echo '[global]' > /etc/pip.conf"
 sudo bash -c "echo 'extra-index-url=https://www.piwheels.org/simple' >> /etc/pip.conf"
 
-# Install telemetrix
-cd $MIRTE_SRC_DIR/mirte-telemetrix-aio
-pip3 install .
-
-# Install Telemtrix4Arduino project
-# TODO: building STM sometimes fails (and/or hangs)
-cd $MIRTE_SRC_DIR/mirte-install-scripts
-mkdir -p /home/mirte/Arduino/libraries
-mkdir -p /home/mirte/arduino_project/Telemetrix4Arduino
-ln -s $MIRTE_SRC_DIR/mirte-telemetrix4arduino /home/mirte/Arduino/libraries/Telemetrix4Arduino
-ln -s $MIRTE_SRC_DIR/mirte-telemetrix4arduino/examples/Telemetrix4Arduino/Telemetrix4Arduino.ino /home/mirte/arduino_project/Telemetrix4Arduino
-
 {
+	# Install telemetrix
+	cd $MIRTE_SRC_DIR/mirte-telemetrix-aio
+	pip3 install .
+} 2>&1 | sed 's/^/telemetrix::: /' &
+{
+	# Install Telemtrix4Arduino project
+	# TODO: building STM sometimes fails (and/or hangs)
+	cd $MIRTE_SRC_DIR/mirte-install-scripts
+	mkdir -p /home/mirte/Arduino/libraries
+	mkdir -p /home/mirte/arduino_project/Telemetrix4Arduino
+	ln -s $MIRTE_SRC_DIR/mirte-telemetrix4arduino /home/mirte/Arduino/libraries/Telemetrix4Arduino
+	ln -s $MIRTE_SRC_DIR/mirte-telemetrix4arduino/examples/Telemetrix4Arduino/Telemetrix4Arduino.ino /home/mirte/arduino_project/Telemetrix4Arduino
+
 	# Install arduino firmata upload script
 	cd $MIRTE_SRC_DIR/mirte-install-scripts
 	./install_arduino.sh
-} &
+} 2>&1 | sed 's/^/arduino::: /' &
 
 {
 	# Install Mirte Python package
 	cd $MIRTE_SRC_DIR/mirte-python
 	pip3 install .
-} &
+} 2>&1 | sed 's/^/mirte-python::: /' &
 
 {
 	# Install Mirte Interface
 	cd $MIRTE_SRC_DIR/mirte-install-scripts
 	./install_web.sh
-} &
+} 2>&1 | sed 's/^/web::: /' &
 
 {
 	# Install Jupyter Notebook
 	cd $MIRTE_SRC_DIR/mirte-install-scripts
 	./install_jupyter_ros.sh
-} &
+} 2>&1 | sed 's/^/jupyter_ros::: /' &
 
 {
 	# Install Mirte ROS packages
 	cd $MIRTE_SRC_DIR/mirte-install-scripts
 	./install_ROS.sh
-} &
+} 2>&1 | sed 's/^/ROS::: /' &
 
 # Install numpy
 pip3 install numpy
@@ -94,9 +95,11 @@ sudo apt install -y bluez joystick
 	cd ../../
 	make html
 	deactivate
-} &
+} 2>&1 | sed 's/^/docs::: /' &
 # Install overlayfs and make sd card read only (software)
-sudo apt install -y overlayroot
+# sudo apt install -y overlayroot
 # Currently only instaling, not enabled
 #sudo bash -c "echo 'overlayroot=\"tmpfs\"' >> /etc/overlayroot.conf"
-wait # wait on all the backgrounded stuff
+echo "Waiting"
+time wait # wait on all the backgrounded stuff
+echo "Done installing"
