c2fb7f56e58081011793884a185b79c346d23545
Arend-Jan(Mirte-42F850) <a.j.vanhilten@tudelft.nl>
2023-11-06 09:55:08 +0000

    Add cmake build and install

--- pam/storepassword.c
+++ pam/storepassword.c
@@ -11,12 +11,11 @@ PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags,
 {
     if (flags == PAM_PRELIM_CHECK)
     {
-        printf("Your password will be stored in plaintext, so don't make it your cats name! The web-login and the Mirte-hotspot will get the same password.");
         return PAM_SUCCESS;
     }
     char *pwd;
 
     int r = pam_get_item(pamh, PAM_AUTHTOK, (const void **)&pwd);
-    printf("Your new password is \"%s\". Very nice!\n", pwd);
+    printf("Your new password is \"%s\". Very nice!\nThe password will be updated for the webpages. The wifi password will be updated at next boot!\n", pwd);
     return PAM_SUCCESS;
 }
