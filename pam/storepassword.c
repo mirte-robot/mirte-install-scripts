b3f1b2e68292475c1db303ab3c49b2089d837dba
Arend-Jan(Mirte-42F850) <a.j.vanhilten@tudelft.nl>
2023-11-06 10:30:53 +0000

    cleanup cmake

--- pam/storepassword.c
+++ pam/storepassword.c
@@ -9,6 +9,7 @@
 PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags,
                                 int argc, const char **argv)
 {
+
     if (flags == PAM_PRELIM_CHECK)
     {
         return PAM_SUCCESS;
@@ -16,6 +17,6 @@ PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags,
     char *pwd;
 
     int r = pam_get_item(pamh, PAM_AUTHTOK, (const void **)&pwd);
-    printf("Your new password is \"%s\". Very nice!\nThe password will be updated for the webpages. The wifi password will be updated at next boot!\n", pwd);
+    printf("Your new password is \"%s\". Very nice, very secure!!\nThe password will be updated for the webpages. The wifi password will be updated at next boot!\n", pwd);
     return PAM_SUCCESS;
 }
