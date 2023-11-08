9f3121f34ba55571b0ac41224dfda51c6a8b4311
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2023-11-08 14:42:40 +0100

    vscode fixes?

--- install_vscode.sh
+++ install_vscode.sh
@@ -8,13 +8,16 @@ wget https://gist.githubusercontent.com/b01/0a16b6645ab7921b0910603dfb85e4fb/raw
 chmod +x download-vs-code-server.sh
 sudo -u mirte $MIRTE_SRC_DIR/vscode/download-vs-code-server.sh
 
+
+cd /home/mirte/
 # For the website:
-wget -O vscode_cli.tar.gz https://az764295.vo.msecnd.net/stable/f1b07bd25dfad64b0167beb15359ae573aecd2cc/vscode_cli_alpine_arm64_cli.tar.gz
-tar -xvf vscode_cli.tar.gz
-./code update # update the server
+sudo -u mirte wget -O vscode_cli.tar.gz https://az764295.vo.msecnd.net/stable/f1b07bd25dfad64b0167beb15359ae573aecd2cc/vscode_cli_alpine_arm64_cli.tar.gz
+sudo -u mirte tar -xvf vscode_cli.tar.gz 
+sudo -u mirte rm vscode_cli.tar.gz
+sudo -u mirte ./code update # update the server
 
 # first load it will trigger a download of the actual server. The Mirtes don't have networking, so download it during sd generation
-./code serve-web --port 9000 --host 0.0.0.0 --without-connection-token --accept-server-license-terms &
+sudo -u mirte ./code serve-web --port 9000 --host 0.0.0.0 --without-connection-token --accept-server-license-terms &
 code_pid=$!
 until [ "$(wget -qO- http://localhost:9000/ | wc --bytes)" -gt "1000" ]; do
 	sleep 5
@@ -22,7 +25,7 @@ done
 
 # Add the license terms after <!--TERMS--> in the vscode/index.html file that the users must accept before using it.
 # rerun vscode with the same port to let it stop immediately, but it will show the license terms
-sudo sed -i '/^<\!--TERMS-->$/r'<(~/code serve-web --port 9000 --host 0.0.0.0 | grep -v error) $MIRTE_SRC_DIR/mirte-install-scripts/sites/vscode/index.html
+sudo sed -i '/^<\!--TERMS-->$/r'<(./code serve-web --port 9000 --host 0.0.0.0 | grep -v error) $MIRTE_SRC_DIR/mirte-install-scripts/sites/vscode/index.html
 
 # Stop the server started earlier
 kill $code_pid
