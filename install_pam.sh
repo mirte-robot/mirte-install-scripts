d96c45fb4d4d707e318d11f734323b137ef3eccb
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2023-11-08 12:07:37 +0100

    Fix install missing cmake

--- install_pam.sh
+++ install_pam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd pam || exit
-sudo apt-get install libpam0g-dev
+sudo apt-get install cmake libpam0g-dev -y
 mkdir build
 cd build || exit 1
 cmake -DCMAKE_BUILD_TYPE=Release .. # will install json-c when not yet installed
