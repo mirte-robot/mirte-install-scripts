e37b71a655638bd48f3b1d10ce67e92c14c3bc4d
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-04-15 17:42:13 +0200

    move to arendjan/telemetrxi4rpipico to download img

--- install_arduino.sh
+++ install_arduino.sh
@@ -79,8 +79,9 @@ rm -rf pico-sdk
 rm -rf picotool
 
 #  Download latest uf2 release, resulting in Telemetrix4RpiPico.uf2
+# TODO:  Downlaods from arendjan/telemetrix4rpipico, as it isn't released yet on the official repo
 cd $MIRTE_SRC_DIR/mirte-install-scripts || exit 1
-curl -s https://api.github.com/repos/mirte-robot/telemetrix4rpipico/releases/latest |
+curl -s https://api.github.com/repos/arendjan/telemetrix4rpipico/releases/latest |
 	grep ".*/Telemetrix4RpiPico.uf2" |
 	cut -d : -f 2,3 |
 	tr -d \" |
