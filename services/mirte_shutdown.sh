7d2509fe3a078fe38b365a1da84ceac022acd927
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2024-04-30 14:05:08 +0000

    fix shutdown service

--- services/mirte_shutdown.sh
+++ services/mirte_shutdown.sh
@@ -2,12 +2,12 @@
 set -xe
 # Don't turn off the relay if the system is rebooting
 # TODO: does not work if ros is not running
-if ! systemctl list-jobs | grep -q -e "reboot.target.*start"; then
-	echo "not rebooting"
-else
-	printf "Rebooting\n"
-	exit
-fi
+# if ! systemctl list-jobs | grep -q -e "reboot.target.*start"; then
+# 	echo "not rebooting"
+# else
+# 	printf "Rebooting\n"
+# 	exit
+# fi
 
 touch /home/mirte/shutdown
 source /home/mirte/mirte_ws/devel/setup.bash
