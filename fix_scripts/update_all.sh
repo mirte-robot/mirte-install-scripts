b0b86cd39d0c0e58419c261373b1e803eb961fd8
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-05-16 11:15:25 +0200

    change to update-all

--- fix_scripts/update_all.sh
+++ fix_scripts/update_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -xe
-mkdir test || true
-cd test
+sudo systemctl stop mirte-ros
+mkdir ~/update_all || true
+cd ~/update_all
 wget -N https://mirte.arend-jan.com/files/telemetrix/release/Telemetrix4RpiPico.uf2.sha256sum
 wget -N https://mirte.arend-jan.com/files/telemetrix/release/Telemetrix4RpiPico.uf2
 wget -N https://mirte.arend-jan.com/files/telemetrix/release/Telemetrix4RpiPico.bin.sha256sum
@@ -11,7 +12,8 @@ sudo picotool load -f Telemetrix4RpiPico.uf2
 sleep 2
 sudo picotool save -f Telemetrix4RpiPico.bin
 sha256sum -c Telemetrix4RpiPico.bin.sha256sum || exit 1
-
+cd ~
+rm -rf ~/update_all
 cd /usr/local/src/mirte/mirte-tmx-pico-aio
 git pull
 pip install .
@@ -19,3 +21,8 @@ pip install aioconsole
 cd ~/mirte_ws/src/mirte-ros-packages/
 git pull
 catkin build
+
+cd /usr/local/src/mirte/mirte-install-scripts/
+git pull
+sudo systemctl daemon-reload
+sudo systemctl start mirte-ros
