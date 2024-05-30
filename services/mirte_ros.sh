7bf538179584596463871d214e3e9506cbc6f154
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-05-30 16:33:38 +0200

    cleanup mirte_ros.sh

--- services/mirte_ros.sh
+++ services/mirte_ros.sh
@@ -2,17 +2,15 @@
 source /home/mirte/mirte_ws/devel/setup.bash
 
 ip=$(hostname -I | awk '{print $1}') #just get the first ip addr
-echo $ip
 if [ "$(echo $ip | wc -w)" -ne 1 ]; then
 	# happens at boot, when the network is not yet up
-	echo "multiple or none, wont use the ip from hostname"
-	echo $ip
+	echo "multiple or none, wont use the ip from hostname:"
 	hostname -I
 else
 	export ROS_IP="$ip"
 	export ROS_MASTER_URI="http://$ip:11311/"
-	echo $ROS_IP
-	echo $ROS_MASTER_URI
+	echo "ROS_IP=$ROS_IP"
+	echo "ROS_MASTER_URI=$ROS_MASTER_URI"
 fi
 # If the robot user wants to add their own config:
 # source /home/mirte/.bashrc
