b6d6c9b2f5dbe09acecccda173be57e8aecd9cd3
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-05-30 14:34:26 +0200

    style fixes power check

--- services/mirte_master_check.sh
+++ services/mirte_master_check.sh
@@ -4,43 +4,46 @@
 SECONDS=0
 WARN_LVL=0
 while true; do
-    OK=false
-    
-    # echo "topics"
-    percentage=$(
-        ( rostopic echo /mirte/power/power_watcher -n2 | grep percentage | tail -1 ) & pid=$!
-        ( sleep 5 && kill -HUP $pid ) 2>/dev/null & watcher=$!
-        wait $pid 2>/dev/null && pkill -HUP -P $watcher
-    )
-    # echo $percentage
-    # echo $( echo $percentage | wc -c)
-    if [ $( echo $percentage | wc -c) -gt 1 ]; then
-        # echo "percentage"
-        percentage=$( echo "$percentage" | awk '{print $NF}' )
-        # echo $percentage
-        if (( $(echo "$percentage > 0.1" |bc -l) )); then
-  OK=true
-fi
+	OK=false
 
-    if $OK; then
-        # echo "latest percentage: "
-        # echo $percentage
-        SECONDS=0 # update time since last ok
-        WARN_LVL=0
-    fi
-    if [ $SECONDS -gt 300 ] && [ $WARN_LVL -eq 0 ]; then
-        wall "No ROS for longer than 5min"
-        WARN_LVL=1
-    fi
-    if [ $SECONDS -gt 600 ] && [ $WARN_LVL -eq 1 ]; then
-        wall "No ROS for longer than 10min, shutting down in 5min"
-        WARN_LVL=2
-    fi
-    if [ $SECONDS -gt 900 ] && [ $WARN_LVL -eq 2 ]; then
-        wall "shutting down"
-        WARN_LVL=3
-        date > /home/mirte/.shutdown_power
-        sudo shutdown now
-    fi
-    sleep 2
+	# echo "topics"
+	percentage=$(
+		(rostopic echo /mirte/power/power_watcher -n2 | grep percentage | tail -1) &
+		pid=$!
+		(sleep 5 && kill -HUP $pid) 2>/dev/null &
+		watcher=$!
+		wait $pid 2>/dev/null && pkill -HUP -P $watcher
+	)
+	# echo $percentage
+	# echo $( echo $percentage | wc -c)
+	if [ "$(echo $percentage | wc -c)" -gt 1 ]; then
+		# echo "percentage"
+		percentage=$(echo "$percentage" | awk '{print $NF}')
+		# echo $percentage
+		if (($(echo "$percentage > 0.1" | bc -l))); then
+			OK=true
+		fi
+	fi
+
+	if $OK; then
+		# echo "latest percentage: "
+		# echo $percentage
+		SECONDS=0 # update time since last ok
+		WARN_LVL=0
+	fi
+	if [ $SECONDS -gt 300 ] && [ $WARN_LVL -eq 0 ]; then
+		wall "No ROS for longer than 5min"
+		WARN_LVL=1
+	fi
+	if [ $SECONDS -gt 600 ] && [ $WARN_LVL -eq 1 ]; then
+		wall "No ROS for longer than 10min, shutting down in 5min"
+		WARN_LVL=2
+	fi
+	if [ $SECONDS -gt 900 ] && [ $WARN_LVL -eq 2 ]; then
+		wall "shutting down"
+		WARN_LVL=3
+		date >/home/mirte/.shutdown_power
+		sudo shutdown now
+	fi
+	sleep 2
 done
