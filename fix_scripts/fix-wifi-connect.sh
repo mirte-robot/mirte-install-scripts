5b21a477c336615e4edadc4563b87422a4f3f50f
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-05-02 13:34:40 +0200

    style fixes

--- fix_scripts/fix-wifi-connect.sh
+++ fix_scripts/fix-wifi-connect.sh
@@ -6,10 +6,10 @@ tmp_dir=$(mktemp -d)
 cd $tmp_dir
 MY_ARCH=$(arch)
 if [[ "$MY_ARCH" == "aarch64" ]]; then
-    wget https://github.com/ArendJan/balena-os-wifi-connect/releases/download/fix-zerotier/wifi-connect-aarch64-unknown-linux-gnu.zip
+	wget https://github.com/ArendJan/balena-os-wifi-connect/releases/download/fix-zerotier/wifi-connect-aarch64-unknown-linux-gnu.zip
 fi
 if [[ "$MY_ARCH" == "???" ]]; then
-    wget https://github.com/ArendJan/balena-os-wifi-connect/releases/download/fix-zerotier/wifi-connect-armv7-unknown-linux-gnueabihf.zip
+	wget https://github.com/ArendJan/balena-os-wifi-connect/releases/download/fix-zerotier/wifi-connect-armv7-unknown-linux-gnueabihf.zip
 fi
 unzip wifi-connect*
 sudo mv wifi-connect /usr/local/sbin
