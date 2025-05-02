0ed6c38d98a280c11331f2873aadf42d4d2cec7f
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-05-02 12:02:35 +0200

    Fix zerotier wifi-connect

--- network_install.sh
+++ network_install.sh
@@ -23,10 +23,15 @@ sudo apt install -y network-manager
 # Install wifi-connect
 MY_ARCH=$(arch)
 if [[ "$MY_ARCH" == "armv7l" ]]; then MY_ARCH="rpi"; fi
-# TODO: This version is from Feb 20, 2023 maybe upgrade soon...
+# TODO: check with armv7 if it works and dynamically download the correct version
 # skip if amd64
 if [[ "$MY_ARCH" != "x86_64" ]]; then
-	wget "https://github.com/balena-os/wifi-connect/releases/download/v4.11.1/wifi-connect-v4.11.1-linux-$(echo "$MY_ARCH").zip"
+	if [[ "$MY_ARCH" == "aarch64" ]]; then
+		wget https://github.com/ArendJan/balena-os-wifi-connect/releases/download/fix-zerotier/wifi-connect-aarch64-unknown-linux-gnu.zip
+	fi
+	if [[ "$MY_ARCH" == "???" ]]; then
+		wget https://github.com/ArendJan/balena-os-wifi-connect/releases/download/fix-zerotier/wifi-connect-armv7-unknown-linux-gnueabihf.zip
+	fi
 	unzip wifi-connect*
 	sudo mv wifi-connect /usr/local/sbin
 	rm wifi-connect*
