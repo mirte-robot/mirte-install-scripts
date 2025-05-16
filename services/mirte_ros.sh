2d782598461fed18599d7e8d8ba7b643c2ff2404
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-05-16 13:35:34 +0200

    Add fastdds discovery support mirte-ros service

--- services/mirte_ros.sh
+++ services/mirte_ros.sh
@@ -14,5 +14,13 @@ if [ "$RMW_IMPLEMENTATION" = "rmw_zenoh_cpp" ]; then
 	sudo pkill -9 -f ^ros && ros2 daemon stop
 	ros2 run rmw_zenoh_cpp rmw_zenohd &
 fi
-
+if [ "$MIRTE_FASTDDS" = "true" ]; then
+	if [ -z "$(ss -l | grep -w "11811")" ]; then
+		echo "starting discovery server on port 11811"
+		# echo "Please run a discovery server on port 11811 with the following command:"
+		fastdds discovery --server-id 0 -p 11811 &
+	fi
+	ros2 daemon stop
+	ros2 daemon start
+fi
 ros2 launch mirte_bringup $LAUNCH_FILE.launch.py
