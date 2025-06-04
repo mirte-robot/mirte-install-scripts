5a6bce59afb3ac0bafa7816fba8c7193ee9eae88
Martin Klomp <m.klomp@tudelft.nl>
2025-06-04 09:44:40 +0200

    Forgot actual patching of the script

--- network_install.sh
+++ network_install.sh
@@ -72,7 +72,7 @@ sed -i 's/#PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 wget https://raw.githubusercontent.com/ev3dev/ev3-systemd/ev3dev-buster/scripts/ev3-usb.sh -P $MIRTE_SRC_DIR/mirte-install-scripts
 sudo chown mirte:mirte $MIRTE_SRC_DIR/mirte-install-scripts/ev3-usb.sh
 chmod +x $MIRTE_SRC_DIR/mirte-install-scripts/ev3-usb.sh
-patch 
+patch $MIRTE_SRC_DIR/mirte-install-scripts/ev3-usb.sh -i $MIRTE_SRC_DIR/mirte-install-scripts/fix_scripts/ev3-usb.patch
 sudo bash -c 'echo "libcomposite" >> /etc/modules'
 # remove g_serial from modules to let the ev3-usb script enable usb ethernet on the orange pi zero 1 as well.
 sudo bash -c "sed -i '/g_serial/d' /etc/modules"
