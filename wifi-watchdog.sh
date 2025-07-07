7d8dc43b38b09a0b068d50a3fbf6cf266a120b1b
Martin Klomp <m.klomp@tudelft.nl>
2025-07-07 10:06:36 +0000

    Reboot after dmesg wifi error

--- wifi-watchdog.sh
+++ wifi-watchdog.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-#echo "begin watchdog"  >  /home/mirte/test.txt
-
-# echo "wifi watchdog service restarted" > /dev/kmsg
-
-# sub="xradio"
-
 dmesg --follow | while read -r line; do
-	if echo "$line" | grep -q "xradio WSM-ERR: CMD timeout!"; then
+        # OrangePi Zero 1: xradio WSM-ERR: CMD timeout!
+        # OrangePi Zero 2: WCN_ERR: dumpmem_rx_callback open
+	if echo "$line" | grep -qE "xradio WSM-ERR: CMD timeout!|WCN_ERR: dumpmem_rx_callback open"; then
 		echo "CRASH! REBOOT!" >/dev/kmsg
-		echo "$line" >>./test.txt
-		#     reboot -f now
+		echo "$line" >> /home/mirte/wifi-watchdog.err
 		echo b >/proc/sysrq-trigger
 	fi
 done
