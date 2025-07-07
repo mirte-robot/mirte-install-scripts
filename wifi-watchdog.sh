e7718fc2f356cc535f16e6fc88c5d18484ca3bb5
Martin Klomp <m.klomp@tudelft.nl>
2025-07-07 10:13:14 +0000

    Fixing shfmt

--- wifi-watchdog.sh
+++ wifi-watchdog.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 dmesg --follow | while read -r line; do
-        # OrangePi Zero 1: xradio WSM-ERR: CMD timeout!
-        # OrangePi Zero 2: WCN_ERR: dumpmem_rx_callback open
+	# OrangePi Zero 1: xradio WSM-ERR: CMD timeout!
+	# OrangePi Zero 2: WCN_ERR: dumpmem_rx_callback open
 	if echo "$line" | grep -qE "xradio WSM-ERR: CMD timeout!|WCN_ERR: dumpmem_rx_callback open"; then
 		echo "CRASH! REBOOT!" >/dev/kmsg
-		echo "$line" >> /home/mirte/wifi-watchdog.err
+		echo "$line" >>/home/mirte/wifi-watchdog.err
 		echo b >/proc/sysrq-trigger
 	fi
 done
