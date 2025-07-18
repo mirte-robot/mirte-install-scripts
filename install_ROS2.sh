037969c602050e09fe2f7e4325ae153897bbfc5f
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-07-18 15:25:37 +0200

    fix install, try docker image

--- install_ROS2.sh
+++ install_ROS2.sh
@@ -16,6 +16,9 @@ sudo apt install software-properties-common -y
 sudo add-apt-repository universe -y
 sudo apt update && sudo apt install curl -y
 ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+if [ -z "$ROS_APT_SOURCE_VERSION" ]; then
+	ROS_APT_SOURCE_VERSION="1.1.0"
+fi
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb"
 sudo apt install /tmp/ros2-apt-source.deb
 sudo apt update
