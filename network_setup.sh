3e2c3ec883b5e94578c094dbeec7af2058cfda67
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-07-23 16:40:19 +0200

    start auto dhcp in background

--- network_setup.sh
+++ network_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MIRTE_SRC_DIR=/usr/local/src/mirte
 
-$MIRTE_SRC_DIR/mirte-install-scripts/services/mirte_auto_dhcp_eth.sh || true
+$MIRTE_SRC_DIR/mirte-install-scripts/services/mirte_auto_dhcp_eth.sh & || true
 
 # wait for wlan0 to be up, with max 30 seconds
 TIMEOUT=30
