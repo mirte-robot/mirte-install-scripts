e6cf68d8ff0f38e4c0ef0081510d7336692b48dc
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-12-08 10:40:47 +0100

    Use auto standby rplidar c1

--- install_ROS2.sh
+++ install_ROS2.sh
@@ -181,7 +181,7 @@ if [[ $MIRTE_TYPE == "mirte-master" ]]; then
 
 	# install lidar and depth camera
 	cd /home/mirte/mirte_ws/src || exit 1
-	git clone https://github.com/Slamtec/rplidar_ros.git --single-branch -b ros2 # FIXME-FUTURE: Can be installed in newer versions if V2.1.5 is released
+	git clone https://github.com/ArendJan/rplidar_ros.git --single-branch -b auto_standby_launch_param_c1 # FIXME-FUTURE: When merged, switch back to official repo
 
 	# git clone https://github.com/ArendJan/ros2_astra_camera.git -b fix-ros-jammy      # compressed images image transport fixes, fork of orbbec/... with also lazy nodes
 	# git clone https://github.com/clearpathrobotics/clearpath_mecanum_drive_controller # FIXME: Can be installed from apt? why build?
