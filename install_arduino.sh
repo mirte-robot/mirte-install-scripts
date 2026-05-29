f0b2658f9de115ca80a367426cee257244d7f92d
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2026-05-29 16:08:25 +0200

    Better telemetrix uf2 download with gh cli

--- install_arduino.sh
+++ install_arduino.sh
@@ -74,19 +74,39 @@ else
 	cd $MIRTE_SRC_DIR/mirte-telemetrix4rpipico || exit 1
 	mkdir -p build || true
 	cd build || exit 1
+	function download_uf2() {
+		gh run list -L 1 -w "Deploy new version" -s "success" --repo $REPO --branch $BRANCH --json databaseId -q ".[0].databaseId" | xargs -I {} gh run download {} --repo $REPO --dir . --name "dist" || true
+	}
 	REPO=$(git config --get remote.origin.url | sed 's/https:\/\/github.com\///' | sed 's/\.git//')
+	ORIG_REPO=$REPO
 	BRANCH=$(git rev-parse --abbrev-ref HEAD)
-	curl -s https://api.github.com/repos/$REPO/releases | jq "[.[] | select ((.target_commitish==\"$BRANCH\"))][0]" | grep -F "browser_download_url" | awk -F\" '{print $4}' | grep '\.uf2$' | wget -i - -O Telemetrix4RpiPico.uf2
-	#also download elf file
-	curl -s https://api.github.com/repos/$REPO/releases | jq "[.[] | select ((.target_commitish==\"$BRANCH\"))][0]" | grep -F "browser_download_url" | awk -F\" '{print $4}' | grep '\.elf$' | wget -i - -O Telemetrix4RpiPico.elf
+
+	# get last run from the current branch, if not found, try to get from main branch, if still not found, fail.
+	download_uf2
+
 	# if not found, try to get from main branch
 	if [ ! -f Telemetrix4RpiPico.uf2 ]; then
-		curl -s https://api.github.com/repos/$REPO/releases | jq "[.[] | select ((.target_commitish==\"main\"))][0]" | grep -F "browser_download_url" | awk -F\" '{print $4}' | grep '\.uf2$' | wget -i - -O Telemetrix4RpiPico.uf2
-		curl -s https://api.github.com/repos/$REPO/releases | jq "[.[] | select ((.target_commitish==\"main\"))][0]" | grep -F "browser_download_url" | awk -F\" '{print $4}' | grep '\.elf$' | wget -i - -O Telemetrix4RpiPico.elf
+		echo "No uf2 found for branch $BRANCH, trying to download from main branch"
+		BRANCH=main
+		download_uf2
 		echo "Downloaded Telemetrix4RpiPico.uf2 from main branch"
 	fi
 	if [ ! -f Telemetrix4RpiPico.uf2 ]; then
-		echo "Failed to download Telemetrix4RpiPico.uf2"
+		echo "Failed to download Telemetrix4RpiPico.uf2 from both branch $BRANCH and main branch, using mirte-robot"
+		REPO=mirte-robot/mirte-telemetrix4rpipico
+		BRANCH=main
+		download_uf2
+	fi
+	if [ ! -f Telemetrix4RpiPico.uf2 ]; then
+		echo "failed to download from action, trying release"
+		gh release download --clobber -D . --pattern "*" --repo $ORIG_REPO || true
+	fi
+	if [ ! -f Telemetrix4RpiPico.uf2 ]; then
+		echo "trying from mirte-robot/mirte-telemetrix4rpipico release"
+		gh release download --clobber -D . --pattern "*" --repo mirte-robot/mirte-telemetrix4rpipico || true
+	fi
+	if [ ! -f Telemetrix4RpiPico.uf2 ]; then
+		echo "Failed to download Telemetrix4RpiPico.uf2 from actions and releases, also mirte-robot, please check the repository $ORIG_REPO for the latest build and upload it to the releases if not already there."
 		exit 1
 	fi
 fi
--- install_mirte.sh
+++ install_mirte.sh
@@ -33,6 +33,8 @@ sudo apt install -y python3 python3-pip python3-setuptools python3.10-venv
 sudo bash -c "echo '[global]' > /etc/pip.conf"
 sudo bash -c "echo 'extra-index-url=https://www.piwheels.org/simple' >> /etc/pip.conf"
 
+./install_github_cli.sh
+
 cd $MIRTE_SRC_DIR/mirte-install-scripts
 ./install_arduino.sh
 
