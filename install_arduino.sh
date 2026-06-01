4c5c3642e0718b2cbf0cd7cd6bbe5287affb1716
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2026-06-01 11:26:55 +0200

    Revert telemetrix uf2 download as gh cli cannot be (easily) used

--- install_arduino.sh
+++ install_arduino.sh
@@ -72,42 +72,16 @@ else
 
 	# download last uf2 from telemetrix pico repo
 	cd $MIRTE_SRC_DIR/mirte-telemetrix4rpipico || exit 1
-	mkdir -p build || true
-	cd build || exit 1
-	function download_uf2() {
-		gh run list -L 1 -w "Deploy new version" -s "success" --repo $REPO --branch $BRANCH --json databaseId -q ".[0].databaseId" | xargs -I {} gh run download {} --repo $REPO --dir . --name "dist" || true
-	}
-	REPO=$(git config --get remote.origin.url | sed 's/https:\/\/github.com\///' | sed 's/\.git//')
-	ORIG_REPO=$REPO
-	BRANCH=$(git rev-parse --abbrev-ref HEAD)
+	# sd-image tools will try to download the correct version to .., otherwise just download the latest and hope for the best (TODO: make it more robust)
+	mkdir -p build/ || true
+	cd build/ || exit 1
+	mv ../../Telemetrix4RpiPico.uf2 . || true
+	mv ../../Telemetrix4RpiPico.elf . || true
+	if [ ! -f Telemetrix4RpiPico.uf2 ]; then
+		# download latest uf2 for pico from github releases
+		curl -s https://api.github.com/repos/mirte-robot/telemetrix4rpipico/releases/latest | grep -F "browser_download_url" | awk -F\" '{print $4}' | grep "Telemetrix4RpiPico.*.uf2" | wget -i - -O Telemetrix4RpiPico.uf2
+		curl -s https://api.github.com/repos/mirte-robot/telemetrix4rpipico/releases/latest | grep -F "browser_download_url" | awk -F\" '{print $4}' | grep "Telemetrix4RpiPico.*.elf" | wget -i - -O Telemetrix4RpiPico.elf
 
-	# get last run from the current branch, if not found, try to get from main branch, if still not found, fail.
-	download_uf2
-
-	# if not found, try to get from main branch
-	if [ ! -f Telemetrix4RpiPico.uf2 ]; then
-		echo "No uf2 found for branch $BRANCH, trying to download from main branch"
-		BRANCH=main
-		download_uf2
-		echo "Downloaded Telemetrix4RpiPico.uf2 from main branch"
-	fi
-	if [ ! -f Telemetrix4RpiPico.uf2 ]; then
-		echo "Failed to download Telemetrix4RpiPico.uf2 from both branch $BRANCH and main branch, using mirte-robot"
-		REPO=mirte-robot/mirte-telemetrix4rpipico
-		BRANCH=main
-		download_uf2
-	fi
-	if [ ! -f Telemetrix4RpiPico.uf2 ]; then
-		echo "failed to download from action, trying release"
-		gh release download --clobber -D . --pattern "*" --repo $ORIG_REPO || true
-	fi
-	if [ ! -f Telemetrix4RpiPico.uf2 ]; then
-		echo "trying from mirte-robot/mirte-telemetrix4rpipico release"
-		gh release download --clobber -D . --pattern "*" --repo mirte-robot/mirte-telemetrix4rpipico || true
-	fi
-	if [ ! -f Telemetrix4RpiPico.uf2 ]; then
-		echo "Failed to download Telemetrix4RpiPico.uf2 from actions and releases, also mirte-robot, please check the repository $ORIG_REPO for the latest build and upload it to the releases if not already there."
-		exit 1
 	fi
 fi
 cd $MIRTE_SRC_DIR/mirte-install-scripts/
