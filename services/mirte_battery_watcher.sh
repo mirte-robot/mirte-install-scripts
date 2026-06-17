78462f4a9d3bee05de990c85d3a7b7a87320de49
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2026-06-17 19:00:59 +0200

    better battery soc check

--- services/mirte_battery_watcher.sh
+++ services/mirte_battery_watcher.sh
@@ -7,7 +7,7 @@ SECONDS=0
 LAST_SECONDS=0
 WARN_LVL=0
 mirte_space=$(cat /etc/hostname | tr '[:upper:]' '[:lower:]' | tr '-' '_')
-
+BATTERY_FILE=/tmp/batteryState
 STOP=false
 trap 'echo "Stopping mirte_master_check"; kill $ECHO_PID; STOP=true' SIGTERM SIGINT
 # ros2 topic echo restarts when the topic appears again, so we can just echo always
@@ -17,17 +17,21 @@ topic=/io/power/power_watcher
 if [ "$MIRTE_USE_MULTIROBOT" = "true" ]; then
 	topic=/$mirte_space/io/power/power_watcher
 fi
-ros2 topic echo $topic sensor_msgs/msg/BatteryState --field percentage >/tmp/batteryState &
+ros2 topic echo $topic sensor_msgs/msg/BatteryState --field percentage >$BATTERY_FILE &
 ECHO_PID=$!
 while ! systemctl list-jobs | grep -q -E 'shutdown.target.*start' && ! $STOP; do
 	OK=false
-
+	TOPIC_EXISTS=false
+	# if file is more than 2 lines, then there is a new reading, otherwise the topic is not publishing
+	if [ "$(wc -l <$BATTERY_FILE)" -gt 2 ]; then
+		TOPIC_EXISTS=true
+	fi
 	# echo "topics"
 	percentage=$(
-		tail -2 /tmp/batteryState | head -1
+		tail -2 $BATTERY_FILE | head -1
 	) || true
 	echo $percentage
-	true >/tmp/batteryState
+	true >$BATTERY_FILE
 	# percentage=$(echo "$percentage" | tail -1)
 	# echo $percentage
 	# echo $( echo $percentage | wc -c)
@@ -37,6 +41,11 @@ while ! systemctl list-jobs | grep -q -E 'shutdown.target.*start' && ! $STOP; do
 		# echo $percentage
 		if (($(echo "$percentage > 0.1" | bc -l))); then
 			OK=true
+		elif (($(echo "$percentage <= 0.1" | bc -l))); then
+			wall "Battery is low, shutting down in 2min"
+			date >>/home/mirte/.shutdown_battery
+			sudo shutdown +2
+			STOP=true
 		fi
 	fi
 
@@ -69,8 +78,9 @@ while ! systemctl list-jobs | grep -q -E 'shutdown.target.*start' && ! $STOP; do
 	if [ $SECONDS -gt 900 ] && [ $WARN_LVL -eq 2 ]; then
 		wall "shutting down"
 		WARN_LVL=3
-		date >/home/mirte/.shutdown_power
+		date >>/home/mirte/.shutdown_power
 		sudo shutdown now
+		STOP=true
 	fi
 	sleep 10
 done
