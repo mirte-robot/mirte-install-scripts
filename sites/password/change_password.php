ba629ed278bbad4f5834998eadb703c9a8410ef2
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2026-09-09 18:14:44 +0200

    password page: style fixes and suggestions chat

--- sites/password/change_password.php
+++ sites/password/change_password.php
@@ -1,12 +1,19 @@
 <?php
 // get POST parameters
 $fields = json_decode(file_get_contents('php://input'), true);
-$old_password = $fields['old_password'];
-$new_password = $fields['new_password'];
+if (!is_array($fields)) {
+    $msg = "Invalid JSON request body.";
+    report();
+    exit;
+}
+$old_password = (string)($fields['old_password'] ?? '');
+$new_password = (string)($fields['new_password'] ?? '');
+
 $ok = false;
 $msg = "";
 function report() {
     global $ok, $msg;
+    header('Content-Type: application/json');
     echo json_encode(array("ok" => $ok, "msg" => $msg));
 }
 
@@ -18,11 +25,20 @@ if (empty($old_password) || empty($new_password)) {
 }
 
 if (strlen($new_password) < 8) {
-    // echo "New password must be at least 8 characters long.";
     $msg = "New password must be at least 8 characters long.";
     report();
     exit;
 }
+
+// only allow letters, numbers, - and _ in the new password, this makes it safe to use in eval.
+ if (!preg_match('/^[a-zA-Z0-9_-]+$/', $new_password)) {
+     $msg = "New password can only contain letters, numbers, - and _ .";
+     report();
+     exit;
+ }
+
+// just make sure old password is also safe to use in eval, although it will be passed to su and sudo, so it can contain any character.
+$old_password = escapeshellarg($old_password);
 
 // set new password for user mirte
 // chpasswd gets mirte:<new_pw> from stdin
